e712c669bf5ef3a9bd67a28111c174eb8f1e513e
Gabriel Naiman <gnaiman@keepcon.com>
2013-06-04 15:36:32 -0300

    Rename spec

--- spec/file_spec.rb
+++ spec/file_spec.rb
@@ -3,8 +3,8 @@ require 'minitest_helper'
 
 describe File do
 
-  it 'Fix size for windows' do
-    File.open(__FILE__, 'r') { |f| f.size.must_equal 183 }
+  it 'Fix size for windows and jruby' do
+    File.open(__FILE__, 'r') { |f| f.size.must_equal 193 }
   end
 
 end
